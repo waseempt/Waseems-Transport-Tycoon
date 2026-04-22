815734eedd2807b074ed181a8494646c61109046
Waseem Petro <waseempetro5@gmail.com>
2026-04-22 10:35:55 +0200

    Added extended tests to TileTest and GameWorldTest

--- core/src/test/java/io/github/transport_tycoon/TileTest.java
+++ core/src/test/java/io/github/transport_tycoon/TileTest.java
@@ -5,6 +5,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TileTest {
 
+
+    @Test
+    void testRoadProperties() {
+        Tile tile = new Tile(2, 3);
+        assertFalse(tile.hasRoad());
+        assertEquals(0, tile.getRoadMask());
+
+        tile.setHasRoad(true);
+        tile.setRoadMask(15);
+
+        assertTrue(tile.hasRoad());
+        assertEquals(15, tile.getRoadMask());
+    }
+
+    @Test
+    void testZoneConnections() {
+        Tile tile = new Tile(2, 3);
+        assertEquals(0, tile.getZoneConnectionMask());
+        assertNull(tile.getParentZone());
+
+        Zone mockZone = new Zone();
+        tile.setZoneConnection(5, mockZone);
+
+        assertEquals(5, tile.getZoneConnectionMask());
+        assertEquals(mockZone, tile.getParentZone());
+    }
+
+    @Test
+    void testIntersectionLinking() {
+        Tile tile = new Tile(2, 3);
+        assertFalse(tile.hasIntersection());
+        assertNull(tile.getIntersection());
+
+        Intersection mockIntersection = new Intersection(15);
+        tile.setIntersection(mockIntersection);
+
+        assertTrue(tile.hasIntersection());
+        assertEquals(mockIntersection, tile.getIntersection());
+    }
+
     @Test
     void testTreeCountConstraints() {
         Tile tile = new Tile(5, 5);
